5ad274c47448bd9e655c07414b811dbd9aa038f9
Darrell Grant <dlsan3000@gmail.com>
2021-05-08 14:49:22 -0400

    add changed files

---
+++ header.php
@@ -0,0 +1,59 @@
+<?php
+    function display_main_content(){
+        <div class="wrapper">
+        <div id="site_title"></div>
+        <div id="img_wrap"></div>
+        <div class="text_wrap"></div>
+        <div id="nav_wrap"></div>
+    </div>
+  
+<?php
+    }
+?>
+<?php
+    function display_first_content(){
+        <div id="wrapper">
+        <div class="text_wrap"></div>
+        <div id="start_wrap"></div>
+    </div>
+  
+<?php
+    }
+?>
+
+
+
+
+
+<?php
+    function display_header() {
+?>
+    <html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <nav>
+            <a href="">Home</a> |
+            <a href="about.html">About</a> |
+            <a href="contact.html">Contact</a> |
+            <a href="further_reading.html">Further Reading</a>
+        </nav> 
+<?php
+    }
+?>
+        
+        
+<?php
+    function display_footer() {
+?>
+        <footer>
+        </footer>
+    </body>
+</html>
+<?php
+    }
+?>
+
+
+
